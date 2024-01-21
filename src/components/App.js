dbf33d0be90fad69116d37bf7e3c2861a752dca2
Bhavi595 <yadavbhavirao@gmail.com>
2024-01-21 20:08:39 +0530

    Version 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
 import React from "react";
 import './../styles/App.css';
+import { Route , Routes } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="about">About</NavLink></li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        
+      </Routes>
     </div>
   )
 }
